3aa42f708e117b6f1988d944aeb5697952e9738d
PakmanGames <im.andy.pak@gmail.com>
2025-11-22 03:32:23 -0500

    Create table schemas for containers and link relationships

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,6 +1,7 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
+import { sql } from "drizzle-orm";
 import { index, pgTableCreator } from "drizzle-orm/pg-core";
 
 /**
@@ -11,6 +12,7 @@ import { index, pgTableCreator } from "drizzle-orm/pg-core";
  */
 export const createTable = pgTableCreator((name) => `HW12_${name}`);
 
+// This is just the default table that came with init of project
 export const posts = createTable(
   "post",
   (d) => ({
@@ -18,9 +20,48 @@ export const posts = createTable(
     name: d.varchar({ length: 256 }),
     createdAt: d
       .timestamp({ withTimezone: true })
-      .$defaultFn(() => /* @__PURE__ */ new Date())
+      .default(sql`NOW()`)
       .notNull(),
     updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
   }),
   (t) => [index("name_idx").on(t.name)],
 );
+
+export const containers = createTable("container", (d) => ({
+  id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+  name: d.varchar({ length: 256 }).notNull(),
+  createdAt: d
+    .timestamp({ withTimezone: true })
+    .default(sql`NOW()`)
+    .notNull(),
+}));
+
+export const statuses = createTable("status", (d) => ({
+  id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+  containerId: d
+    .integer()
+    .notNull()
+    .references(() => containers.id, { onDelete: "cascade" }),
+  status: d.varchar({ length: 256 }).notNull(),
+  checkedInAt: d
+    .timestamp({ withTimezone: true })
+    .default(sql`NOW()`)
+    .notNull(),
+}));
+
+export const errors = createTable("error", (d) => ({
+  id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+  containerId: d
+    .integer()
+    .notNull()
+    .references(() => containers.id, { onDelete: "cascade" }),
+  errorMessage: d.varchar({ length: 1024 }).notNull(),
+  explaination: d.varchar({ length: 2048 }).notNull(),
+  suggestedFix: d.varchar({ length: 2048 }).notNull(),
+  occurredAt: d
+    .timestamp({ withTimezone: true })
+    .default(sql`NOW()`)
+    .notNull(),
+}));
+
+// User and organization tables might not be necessary lets wait for clerk auth implementation first
